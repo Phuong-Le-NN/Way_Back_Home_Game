3f29bb7ea300d56c13293a10411fd5675218c684
Annie Rose Pfeufer <pfeufe_a1@denison.edu>
2025-03-27 16:16:20 -0400

    reference for our final game

---
+++ 2D_Platformer/Assets/Scripts/BottomPlatform.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomPlatform : MonoBehaviour {
+    public float moveDistance = 5f;  // Total distance the platform moves back and forth
+    public float speed = 1f;         // Speed of the movement
+
+    private Vector3 startPos;
+
+    void Start() {
+        startPos = transform.position;
+    }
+
+    void Update() {
+        // Use Mathf.PingPong to create a smooth back-and-forth movement
+        float x = Mathf.PingPong(Time.time * speed, moveDistance);
+        transform.position = new Vector3(startPos.x + x, transform.position.y, transform.position.z);
+    }
+}
