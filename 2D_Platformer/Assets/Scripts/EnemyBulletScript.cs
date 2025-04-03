04bb3f5c29f1702906b42bbcbcb4017337fa6bfc
Oghap Kim <kim_o1@denison.edu>
2025-04-03 01:12:44 -0400

    in progress of enemy scene

---
+++ 2D_Platformer/Assets/Scripts/EnemyBulletScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+    private GameObject Player;
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
