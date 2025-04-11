f03e0982a4a95c82ab3026ad7be9e94fb28c54bf
cavali-c1 <cavali_c1@denison.edu>
2025-04-10 20:45:36 -0400

    added replayScene to locked door level

---
+++ 2D_Platformer/Assets/PlayerManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static bool isGameOver;
+    public GameObject gameOverScreen;
+
+    public void Awake()
+    {
+        isGameOver = false;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGameOver)
+        {
+            gameOverScreen.SetActive(true);
+
+        }
+    }
+
+    public void ReplayLevel()
+    {
+        SceneManager.LoadScene("LockedDoor");
+    }
+}
