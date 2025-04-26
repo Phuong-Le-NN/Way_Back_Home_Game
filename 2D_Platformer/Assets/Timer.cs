ddd38ef69d21885eed1bb0ff87fe5b68b8e472f0
cavali-c1 <cavali_c1@denison.edu>
2025-04-26 15:56:55 -0400

    added timer and panel

--- 2D_Platformer/Assets/Timer.cs
+++ 2D_Platformer/Assets/Timer.cs
@@ -1,25 +1,31 @@
 using UnityEngine;
 using TMPro;
+
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
+    [SerializeField] GameObject gameOverPanel; 
 
-  
-
-    // Update is called once per frame
+     void Start()
+    {
+        gameOverPanel.SetActive(false); // Hide the Game Over panel when the level starts
+    }
     void Update()
     {
-        if(remainingTime > 0)
+        if (remainingTime > 0)
         {
             remainingTime -= Time.deltaTime;
         }
-        else if (remainingTime < 0)
+        else if (remainingTime <= 0)
         {
             remainingTime = 0;
+            gameOverPanel.SetActive(true);
         }
+
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
     }
 }
+
