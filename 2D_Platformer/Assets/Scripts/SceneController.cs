e25f2a625d531b39129da820359c5c52d1d82268
Phuong-Le-NN <le_p2@denison.edu>
2025-04-28 17:30:33 -0400

    fix Y distance platform

--- 2D_Platformer/Assets/Scripts/SceneController.cs
+++ 2D_Platformer/Assets/Scripts/SceneController.cs
@@ -47,7 +47,7 @@ public class SceneController : MonoBehaviour {
                 platform.SetPlatform(id, images[id]);
 
                 offsetX = Random.Range(4f, 6f);
-                offsetY = Random.Range(1f, 2f); 
+                offsetY = Random.Range(1.2f, 1.8f); 
                 if ((end.transform.position.x > posX)){
                     posX = posX + offsetX;
                 }
