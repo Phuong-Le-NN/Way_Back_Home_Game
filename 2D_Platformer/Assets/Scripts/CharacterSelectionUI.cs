0f7f7ee4b28264ff2e426b28e5fef0e6d91a018c
oghapkim <kim_o1@denison.edu>
2025-05-06 12:16:33 -0400

    working character selection with ability displayed

--- 2D_Platformer/Assets/Scripts/CharacterSelectionUI.cs
+++ 2D_Platformer/Assets/Scripts/CharacterSelectionUI.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
 public class CharacterSelectionUI : MonoBehaviour
 {
     public GameObject optionPrefab;
-
     public Transform prevCharacter;
     public Transform selectedCharacter;
 
@@ -15,31 +13,45 @@ public class CharacterSelectionUI : MonoBehaviour
             GameObject option = Instantiate(optionPrefab, transform);
             Button button = option.GetComponent<Button>();
 
-            button.onClick.AddListener(() =>{
+            button.onClick.AddListener(() => {
                 StartGameManager.instance.SetCharacter(c);
-                if(selectedCharacter != null){
+
+                if (selectedCharacter != null){
                     prevCharacter = selectedCharacter;
                 }
 
-                selectedCharacter = option.transform; 
-                SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1)%6);
+                selectedCharacter = option.transform;
+                SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % 6);
             });
 
+            // ✅ Update name text to include ability info
             Text text = option.GetComponentInChildren<Text>();
-            text.text = c.name;
+            text.text = c.name + "\n" + GetAbilityInfo(c.name);
 
             Image image = option.GetComponentInChildren<Image>();
             image.sprite = c.icon;
         }
     }
+
     private void Update(){
         if (selectedCharacter != null){
-            selectedCharacter.localScale = Vector3.Lerp(selectedCharacter.localScale, 
-            new Vector3(1.2f, 1.2f, 1.2f), Time.deltaTime * 10);
+            selectedCharacter.localScale = Vector3.Lerp(selectedCharacter.localScale,
+                new Vector3(1.2f, 1.2f, 1.2f), Time.deltaTime * 10);
         }
         if (prevCharacter != null){
-            prevCharacter.localScale = Vector3.Lerp(prevCharacter.localScale, 
-            new Vector3(1f, 1f, 1f), Time.deltaTime * 10);
+            prevCharacter.localScale = Vector3.Lerp(prevCharacter.localScale,
+                new Vector3(1f, 1f, 1f), Time.deltaTime * 10);
+        }
+    }
+
+    // ✅ Ability info based on character name
+    private string GetAbilityInfo(string name){
+        if (name == "Corgi"){
+            return "Speed: +++\nHealth: +++++";
+        } else if (name == "Dalmatian"){
+            return "Speed: +++++\nHealth: +++";
+        } else {
+            return "Speed: N/A\nHealth: N/A";
         }
     }
 }
